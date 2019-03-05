38961aa95fff9d7b30a257c814ee9621d104f51f
Michael Milewski <saramic@gmail.com>
2019-03-05 19:04:30 +1100

    :chart_with_downwards_trend: use posgres for heroku
    should have --database=postgresql and probably --skip-test
    
    maybe even --api

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_03_05_073959) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "messages", force: :cascade do |t|
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
